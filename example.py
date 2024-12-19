4e6840741eb8b140fe01cb88c4b6581e0cf42b54
Dmitry Ledentsov <dmlled@yahoo.com>
2024-12-19 20:44:23 +0100

    more readable python

--- example.py
+++ example.py
@@ -8,11 +8,6 @@ from term import Atom
 from pyrlang.node import Node
 from pyrlang.process import Process
 
-logging.basicConfig(
-    stream=sys.stdout,
-    level=logging.INFO,
-    format="%(asctime)s [%(name)s]: %(message)s",
-    datefmt="%Y-%m-%dT%H:%M:%S%z")
 LOG = logging.getLogger()
 
 class MyProcess(Process):
@@ -20,16 +15,32 @@ class MyProcess(Process):
         self.count = 0
         self.erlang_node = os.getenv("ERLANG_NODE", 'erl@127.0.0.1')
         LOG.info(f"ERLANG_NODE={self.erlang_node}")
+
         Process.__init__(self)
+
+        LOG.info("Registering process 'my_process' on this node")
         self.get_node().register_name(self, Atom('my_process'))
-        LOG.info("Registering process - 'my_process'")
 
     def handle_one_inbox_message(self, msg):
         LOG.info("received %s", msg)
+
         self.get_node().send_nowait(sender=self.pid_,
                   receiver=(Atom(self.erlang_node), Atom('hello')),
                   message=(Atom('hello_from_python'),self.pid_, self.count))
+        
         self.count = self.count + 1
+
+def set_up_logging():
+    logging.basicConfig(
+        stream=sys.stdout,
+        level=logging.INFO,
+        format="%(asctime)s [%(name)s]: %(message)s",
+        datefmt="%Y-%m-%dT%H:%M:%S%z"
+    )
+
+    # reduce low-level logging
+    logging.getLogger("pyrlang.dist_proto.base_dist_protocol").setLevel(logging.CRITICAL)
+    logging.getLogger("pyrlang.node").setLevel(logging.CRITICAL)
 
 
 async def main():
@@ -37,12 +48,11 @@ async def main():
     my_node = os.getenv("MY_NODE", "py@127.0.0.1")
     LOG.info(f"MY_NODE={my_node}")
     n = Node(node_name=my_node, cookie="COOKIE")
+    set_up_logging()
 
-    # reduce low-level logging
-    logging.getLogger("pyrlang.dist_proto.base_dist_protocol").setLevel(logging.CRITICAL)
-    logging.getLogger("pyrlang.node").setLevel(logging.CRITICAL)
-
+    # start the named async process that accepts incoming messages
     MyProcess()
+
     await sleep(20)
     LOG.info("stopped the python node")
 
